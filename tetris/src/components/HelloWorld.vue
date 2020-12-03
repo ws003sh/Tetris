6880499fd18ae4b1ebf293ee5686c932b81a0832
邹佳宏 <zsggzh@outlook.com>
2020-12-03 21:57:55 +0800

    save

--- tetris/src/components/HelloWorld.vue
+++ tetris/src/components/HelloWorld.vue
@@ -6,6 +6,14 @@
      {{j}}
    </div>
   </div>
+  <div class="control">
+    <p>{{ currentKey }}</p>
+    <button @click="start">start</button>
+    <button @keyup.up="handleKeyUp('up')">up</button>
+    <button @keyup.down="handleKeyUp('down')">down</button>
+    <button @keyup.left="handleKeyUp('left')">left</button>
+    <button @keyup.right="handleKeyUp('right')">right</button>
+  </div>
 </div>
 </template>
 
@@ -21,7 +29,15 @@ export default {
       }
     }
   },
+  created () {
+    let self = this
+    document.addEventListener('keyup', self.handleKeyUp)
+  },
   methods: {
+    handleKeyUp (e) {
+      console.log(e.key)
+      this.currentKey = e.key
+    },
     generateList () {
       let i = 0
       let j = 0
@@ -34,6 +50,37 @@ export default {
         this.list.push(row)
         j++
       }
+    },
+    timeOut (count) {
+      if (count < 21) {
+        count++
+        let self = this
+        this.timer = setTimeout(function () {
+          clearTimeout()
+          this.timer = ''
+          let a = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0, 0]
+          let b = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0 ,0]
+          let normal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0]
+          self.$set(self.list, count - 1, normal)
+          self.$set(self.list, count, normal)
+          self.$set(self.list, count, a)
+          self.$set(self.list, count+1, b)
+          self.timeOut(count)
+        },
+        750)
+      } else {
+        clearTimeout()
+        this.timer = ''
+      }
+    },
+    start () {
+      console.log('start')
+      // 放入一个方块，然后让其落下
+      let a = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0, 0]
+      let b = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0 ,0]
+      this.$set(this.list, 0, a)
+      this.$set(this.list, 1, b)
+      this.timeOut(0)
     }
   },
   mounted () {
@@ -41,7 +88,9 @@ export default {
   },
   data () {
     return {
-      list: []
+      list: [],
+      currentKey: '',
+      timer: ''
     }
   }
 }
@@ -54,7 +103,7 @@ export default {
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  width: 400px;
+  /* width: 400px; */
   border: 1px solid;
   border-bottom: 0;
 }
@@ -64,13 +113,21 @@ export default {
   display: flex;
 }
 .item {
-  padding: 10px;
+  padding: 5px;
+  width: 20px;
+  height: 20px;
+  border: 1px solid;
+  border-bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .index {
-  padding: 10px;
+  padding: 5px;
   border-right: 1px solid;
 }
 .haveValue {
-  background-color: #666;
+  background-color: rgb(235, 190, 94);
+  color: white;
 }
 </style>
