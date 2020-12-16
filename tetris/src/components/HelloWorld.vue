23cf3a2629df9639d6ba857a21927a518733882b
邹佳宏 <zsgzzh@outlook.com>
2020-12-16 11:06:58 +0800

    添加更多图形，支持键盘操控

--- tetris/src/components/HelloWorld.vue
+++ tetris/src/components/HelloWorld.vue
@@ -1,18 +1,39 @@
 <template>
+<div class="view">
 <div class="space">
-  <div class="row" v-for="(i, index) in list" :key="i.id">
+  <div class="row" v-for="(i, index) in matrix" :key="i.id">
     <span class="index">{{ index + 1 | fixIndex }}</span>
    <div v-for="j in i" :key="j.id" :class=" j === 0 ? 'item' : 'item haveValue'">
      {{j}}
    </div>
   </div>
-  <div class="control">
-    <p>{{ currentKey }}</p>
-    <button @click="start">start</button>
-    <button @keyup.up="handleKeyUp('up')">up</button>
-    <button @keyup.down="handleKeyUp('down')">down</button>
-    <button @keyup.left="handleKeyUp('left')">left</button>
-    <button @keyup.right="handleKeyUp('right')">right</button>
+</div>
+<div class="control">
+  <div>
+    <p>
+      <span>当前按钮：{{ currentKey }}</span>
+      <br>
+      <span>当前图形：{{ graphicalType }}</span>
+    </p>
+    <div>
+      <button @click="start(0, 0)">start</button>
+      <button @click="reset">reset</button>
+      <button @keyup.up="handleKeyUp('up')">up</button>
+      <button @keyup.down="handleKeyUp('down')">down</button>
+      <button @keyup.left="handleKeyUp('left')">left</button>
+      <button @keyup.right="handleKeyUp('right')">right</button>
+    </div>
+  </div>
+    <div>
+      <p>设置类型</p>
+      <div>
+        <button @click="changeGraphical('田')">田</button>
+        <button @click="changeGraphical('I')">I</button>
+        <button @click="changeGraphical('L')">L</button>
+        <button @click="changeGraphical('Z')">Z</button>
+        <button @click="changeGraphical('土')">土</button>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -34,63 +55,142 @@ export default {
     document.addEventListener('keyup', self.handleKeyUp)
   },
   methods: {
-    handleKeyUp (e) {
-      console.log(e.key)
-      this.currentKey = e.key
+    reset () {
+      this.matrix = []
+      this.generateMatrix()
     },
-    generateList () {
+    handleKeyUp (e) {
+      // console.log(e)
+      this.currentKey = e.key
+      switch (e.key) {
+        case 'ArrowRight':
+          this.beforeMove()
+          this.xOffset++
+          this.move()
+          break
+        case 'ArrowLeft':
+          this.beforeMove()
+          this.xOffset--
+          this.move()
+          break
+        case 'ArrowUp':
+          this.beforeMove()
+          this.yOffset--
+          this.move()
+          break
+        case 'ArrowDown':
+          this.beforeMove()
+          this.yOffset++
+          this.move()
+          break
+      }
+    },
+    generateMatrix () {
+      // 生成矩阵
       let i = 0
       let j = 0
-      let row = []
-      while (i < 12) {
-        row.push(0)
-        i++
-      }
       while (j < 20) {
-        this.list.push(row)
+        let row = []
+        while (i < 12) {
+          row.push(0)
+          i++
+        }
+        this.matrix.push(row)
         j++
-      }
-    },
-    timeOut (count) {
-      if (count < 21) {
-        count++
-        let self = this
-        this.timer = setTimeout(function () {
-          clearTimeout()
-          this.timer = ''
-          let a = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0, 0]
-          let b = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0 ,0]
-          let normal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0]
-          self.$set(self.list, count - 1, normal)
-          self.$set(self.list, count, normal)
-          self.$set(self.list, count, a)
-          self.$set(self.list, count+1, b)
-          self.timeOut(count)
-        },
-        750)
-      } else {
-        clearTimeout()
-        this.timer = ''
+        i = 0
       }
     },
     start () {
-      console.log('start')
-      // 放入一个方块，然后让其落下
-      let a = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0, 0]
-      let b = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0 ,0 ,0]
-      this.$set(this.list, 0, a)
-      this.$set(this.list, 1, b)
-      this.timeOut(0)
+      // 放入一个方块
+      switch (this.graphicalType) {
+        case '田':
+          this.showGraphicalMatrix = this.田
+          break
+        case '土':
+          this.showGraphicalMatrix = this.土
+          break
+        case 'L':
+          this.showGraphicalMatrix = this.L
+          break
+        case 'Z':
+          this.showGraphicalMatrix = this.Z
+          break
+        case 'I':
+          this.showGraphicalMatrix = this.I
+          break
+      }
+      let yOffset = 0
+      let xOffset = 4
+      this.showGraphicalMatrix.forEach((i, index) => {
+        i.forEach((j, jIndex) => {
+          if (j === 1) {
+            this.matrix[yOffset + index].splice(xOffset + jIndex, 1, 1)
+          }
+        })
+      })
+    },
+    beforeMove () {
+      // 移动前 删除对应的数据
+      console.log('beforeMove')
+      this.showGraphicalMatrix.forEach((i, index) => {
+        i.forEach((j, jIndex) => {
+          if (j === 1) {
+            this.matrix[this.yOffset + index].splice(this.xOffset + jIndex, 1, 0)
+          }
+        })
+      })
+    },
+    move () {
+      // 移动方块
+      console.log('move')
+      this.showGraphicalMatrix.forEach((i, index) => {
+        i.forEach((j, jIndex) => {
+          if (j === 1) {
+            this.matrix[this.yOffset + index].splice(this.xOffset + jIndex, 1, 1)
+          }
+        })
+      })
+    },
+    changeGraphical (name) {
+      this.graphicalType = name
     }
   },
   mounted () {
-    this.generateList()
+    this.generateMatrix()
   },
   data () {
     return {
-      list: [],
-      currentKey: '',
-      timer: ''
+      matrix: [], // 矩阵 宽12 * 高20
+      currentKey: '', // 当前按下的按键是
+      timer: '', // 计时器
+      graphicalType: '田', // 当前放入的对象类型是 方块
+      xOffset: 4,
+      yOffset: 0,
+      showGraphicalMatrix: [],
+      土: [
+        [0, 1, 0],
+        [1, 1, 1]
+      ],
+      L: [
+        [1, 0],
+        [1, 0],
+        [1, 0],
+        [1, 1]
+      ],
+      I: [
+        [1],
+        [1],
+        [1],
+        [1]
+      ],
+      田: [
+        [1, 1],
+        [1, 1]
+      ],
+      Z: [
+        [1, 1, 0],
+        [0, 1, 1]
+      ]
     }
   }
 }
@@ -129,5 +229,23 @@ export default {
 .haveValue {
   background-color: rgb(235, 190, 94);
   color: white;
+}
+.view {
+  display: flex;
+}
+.control {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid;
+}
+.control div {
+  margin-bottom: 20px;
+}
+.control button {
+  color: white;
+  background-color: red;
+  border: 1px solid white;
+  width: 60px;
+  height: 30px;
 }
 </style>
